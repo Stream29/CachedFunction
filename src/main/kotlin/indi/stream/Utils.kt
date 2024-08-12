f67ad6437e2050e52857553740679209eea86c18
Stream <1542763342@qq.com>
2024-08-12 19:36:53 +0800

    feat: function.cached(containerProvider)

--- src/main/kotlin/indi/stream/Utils.kt
+++ src/main/kotlin/indi/stream/Utils.kt
@@ -1,7 +1,7 @@
 package indi.stream
 
 fun <T : Any, R : Any> ((T) -> R).cached(containerProvider: () -> MutableMap<T, R>) =
-    cached(container())
+    cached(containerProvider())
 
 fun <T : Any, R : Any> ((T) -> R).cached(container: MutableMap<T, R>) =
     { arg: T -> container.getOrPut(arg) { this(arg) } }
