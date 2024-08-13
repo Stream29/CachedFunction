a996875145c711e330da9fec3fbbaab6a8cfe824
Stream <1542763342@qq.com>
2024-08-13 11:17:54 +0800

    feat: infer the return type for kotlin compiler explicitly

--- src/main/kotlin/indi/stream/Utils.kt
+++ src/main/kotlin/indi/stream/Utils.kt
@@ -1,7 +1,7 @@
 package indi.stream
 
-fun <T : Any, R : Any> ((T) -> R).cached(containerProvider: () -> MutableMap<T, R>) =
+fun <T : Any, R : Any> ((T) -> R).cached(containerProvider: () -> MutableMap<T, R>): ((T) -> R) =
     cached(containerProvider())
 
-fun <T : Any, R : Any> ((T) -> R).cached(container: MutableMap<T, R>) =
+fun <T : Any, R : Any> ((T) -> R).cached(container: MutableMap<T, R>): ((T) -> R) =
     { arg: T -> container.getOrPut(arg) { this(arg) } }
